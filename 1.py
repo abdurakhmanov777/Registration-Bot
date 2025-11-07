76e3c7e03c3dadcad20c77a6595dd88e9234351a
abdurakhmanov777 <dalgat03@yandex.ru>
2025-11-08 01:13:18 +0300

    Edited

--- 1.py
+++
@@ -1,7 +0,0 @@
-from app.core.config import FONT_PATH, IMAGE_PATH, LOG_FILE
-from app.core.config.google_sheets import GSHEET_CREDS
-
-print(IMAGE_PATH)
-print(FONT_PATH)
-print(LOG_FILE)
-print(GSHEET_CREDS)
