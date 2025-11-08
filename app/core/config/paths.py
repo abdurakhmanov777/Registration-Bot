07fc52ac5ff664d1a21e640853b8cef196d97641
abdurakhmanov777 <dalgat03@yandex.ru>
2025-11-08 03:59:47 +0300

    Edited

--- app/core/config/paths.py
+++ app/core/config/paths.py
@@ -15,11 +15,11 @@ load_dotenv()
 # ------------------------------------------------------------
 # Основные директории проекта
 # ------------------------------------------------------------
-ASSETS_DIR: Path = BASE_DIR / "app" / "assets"        # Папка с ресурсами
-IMAGES_DIR: Path = ASSETS_DIR / "images"             # Картинки
-FONTS_DIR: Path = ASSETS_DIR / "fonts"              # Шрифты
-FILES_DIR: Path = ASSETS_DIR / "files"              # Документы
-LOCALIZATION_DIR: Path = ASSETS_DIR / "localization"  # Локализация
+ASSETS_DIR: Path = BASE_DIR / "app" / "assets"          # Папка с ресурсами
+IMAGES_DIR: Path = ASSETS_DIR / "images"                # Картинки
+FONTS_DIR: Path = ASSETS_DIR / "fonts"                  # Шрифты
+DOCUMENTS_DIR: Path = ASSETS_DIR / "documents"          # Документы
+LOCALIZATIONS_DIR: Path = ASSETS_DIR / "localizations"  # Локализация
 
 
 # Фоновое изображение
@@ -29,10 +29,10 @@ IMAGE_PATH: Path = IMAGES_DIR / os.getenv("IMAGE_NAME", "background.png")
 FONT_PATH: Path = FONTS_DIR / os.getenv("FONT_NAME", "ALS_Sector_Bold.ttf")
 
 # Анкета гостя
-GUEST_PATH: Path = FILES_DIR / os.getenv("GUEST_PATH", "Анкета_гостя.pdf")
+GUEST_PATH: Path = DOCUMENTS_DIR / os.getenv("GUEST_PATH", "Анкета_гостя.pdf")
 
 # Расписка участника
-PARTICIPANT_PATH: Path = FILES_DIR / os.getenv(
+PARTICIPANT_PATH: Path = DOCUMENTS_DIR / os.getenv(
     "PARTICIPANT_PATH", "Расписка_участника.pdf"
 )
 
--- app/services/localization/loader.py
+++ app/services/localization/loader.py
@@ -4,15 +4,15 @@ from typing import Any
 
 from loguru import logger
 
-from app.core.config import LOCALIZATION_DIR
+from app.core.config import LOCALIZATIONS_DIR
 from app.services.localization.model import Localization
 
-print(LOCALIZATION_DIR)
+
 async def load_localization_main(language: str) -> Localization:
     """
     Загружает файл локализации по указанному языку.
     """
-    file_path: Path = LOCALIZATION_DIR / f"{language}.json"
+    file_path: Path = LOCALIZATIONS_DIR / f"{language}.json"
 
     if not file_path.exists():
         logger.error(f"Localization file not found: {file_path.resolve()}")
